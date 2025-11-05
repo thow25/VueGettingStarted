b5a601881a8c39924fc5c0b3588a97f492e41a00
thow25 <thow25@yahoo.com>
2025-11-05 15:44:47 -0500

    Adding model and css binding. Tried to add methods, watchers and computed values

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,37 @@ import App from './App.vue'
 createApp(App).mount('#app')
 
 const message = Vue.createApp({
+  // Data To Be Used In The Vue App
   data() {
     return {
-      message: 'Binding Vue Data with Text Interpolation!',
+      textMessage: 'Binding Vue Data with Text Interpolation!',
       vueAppClass: 'pinkBG',
       showOriginalMessage: true,
       errorMessage: 'An error has occurred. Unable to display the original message.',
       listOfShapes: ['Circle', 'Square', 'Triangle', 'Hexagon', 'Octagon'],
+      inputText: '',
+      didUserInputText: false,
     }
   },
+  // TODO: Get this working
+  // Methods To Be Used In The Vue App
+  // methods: {
+  //   updateMessage() {
+  //     this.textMessage = 'Hello from Vue!'
+  //   },
+  // },
+  // Watchers To Be Used In The Vue App
+  // watch: {
+  //   inputText(inputText.length > 0) {
+  //     this.didUserInputText = true
+  //   },
+  // },
+  // Computed Properties To Be Used In The Vue App
+  // computed: {
+  //   textMessage() {
+  //     return this.inputText
+  //   },
+  // },
 })
 
 message.mount('#message')
